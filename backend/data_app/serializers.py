7b1d6c58f406c1087b800d651d04ca4ea3ac2c2f
Konrad <amazing0possible@gmail.com>
2024-10-29 15:38:31 +0800

    fix id in serializers.py

--- backend/data_app/serializers.py
+++ backend/data_app/serializers.py
@@ -6,7 +6,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserProfile
-        fields = ['user_id', 'user_name', 'user_job', 'user_contact', 'user_password', 'user_dob_year', 'user_dob_month',
+        fields = ['id', 'user_name', 'user_job', 'user_contact', 'user_password', 'user_dob_year', 'user_dob_month',
                   'user_dob_day', 'user_gender', 'user_custom_gender', 'user_hobbies']
         extra_kwargs = {'user_password': {'write_only': True}}
 
