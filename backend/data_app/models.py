dc03c97f2c02ed229faa4e5e5d043c8e4e374905
konrad <amazing0possible@gmail.com>
2024-11-08 16:38:38 +0800

    modify ep: register -> add user_characters field

--- backend/data_app/models.py
+++ backend/data_app/models.py
@@ -13,6 +13,7 @@ class UserProfile(models.Model):
     user_gender = models.CharField(max_length=50)
     user_custom_gender = models.CharField(max_length=100, blank=True, null=True)
     user_hobbies = models.JSONField(default=list)
+    user_characters = models.JSONField(default=list)
 
     USERNAME_FIELD = 'user_contact'
 
